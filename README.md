e1907d53628f2bc8be1f5480c093a978dc88abec
Justin Harwood <justin.harwood@benefitfocus.com>
2018-02-22 14:26:21 -0500

    port/dir params

---
+++ README.md
@@ -0,0 +1,7 @@
+`goserv [port] [dir]`
+
+e.g.
+
+`goserv 3000 /Users/username/Desktop`
+
+`goserv 1993 .`
--- serve.go
+++ serve.go
@@ -4,13 +4,30 @@ import (
   "net/http"
   "log"
   "os"
+  "fmt"
 )
 
 func main() {
   args := os.Args
-  fs := http.FileServer(http.Dir(args[1]))
-  http.Handle("/", fs)
 
-  log.Println("Listening on 3000...")
-  http.ListenAndServe(":3000", nil)
+  if len(args) < 3 {
+    fmt.Println("arg count < 3")
+  } else {
+
+    port := args[1]
+    dir := args[2]    
+
+    fs := http.FileServer(http.Dir(dir))
+    http.Handle("/", fs)
+
+    log.Println("Serving " + dir + " on port " + port)
+    http.ListenAndServe(":" + port, nil)
+
+  }
+
+  // fs := http.FileServer(http.Dir(args[1]))
+  // http.Handle("/", fs)
+
+  // log.Println("Listening on 3000...")
+  // http.ListenAndServe(":3000", nil)
 }
