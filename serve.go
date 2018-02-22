e37cc05eb3e616cce75ae7c91a391375066bc15b
Justin Harwood <justin.harwood@benefitfocus.com>
2018-02-22 14:27:54 -0500

    clean up

--- serve.go
+++ serve.go
@@ -1,33 +1,21 @@
 package main
 
 import (
-  "net/http"
-  "log"
-  "os"
   "fmt"
+  "log"
+  "net/http"
+  "os"
 )
 
 func main() {
   args := os.Args
-
   if len(args) < 3 {
     fmt.Println("arg count < 3")
   } else {
-
     port := args[1]
     dir := args[2]    
-
     fs := http.FileServer(http.Dir(dir))
     http.Handle("/", fs)
-
     log.Println("Serving " + dir + " on port " + port)
     http.ListenAndServe(":" + port, nil)
-
   }
-
-  // fs := http.FileServer(http.Dir(args[1]))
-  // http.Handle("/", fs)
-
-  // log.Println("Listening on 3000...")
-  // http.ListenAndServe(":3000", nil)
-}
